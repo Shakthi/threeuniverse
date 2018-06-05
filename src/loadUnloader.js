e5ebe6dd3ab6b38ffc6cb1cda9afd71907ab27bc
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-06-05 22:47:24 +0530

    Load script improvised

--- src/loadUnloader.js
+++ src/loadUnloader.js
@@ -1,19 +1,61 @@
 
 import loadScript from "simple-load-script";
 
+let num = 0;
+let callbackMap = {};
+
+
+export function loadnExecute(url, initializer, callback) {
+    num++;
+
+    callbackMap[num] = callback;
+    window[initializer] = function () {
+
+        let attrs = document.currentScript.attributes;
+
+        let scriptIndex;
+
+
+        for (var i = attrs.length - 1; i >= 0; i--) {
+            if (attrs[i].name === "data-scriptid") {
+                scriptIndex = attrs[i].value;
+                break;
+            }
+
+        }
+
+        callbackMap[scriptIndex].apply(null,arguments);
+        delete callbackMap[scriptIndex];
+
+
+    }
+
+    loadScript(url, {
+        removeScript: true, attrs: {
+            "data-scriptid": `${num}`
+        }
+    }).then(element => {
+       //console.log(`Loaded ${url}`);
+    });
+
+
+
+}
 
 
 
 
 
-
-loadScript('https://shakthi.github.io/ThreeJSUniverse/src/sampleMeshModule.js',
-{removeScript:true}).then(element=>{
-    console.log("Loaded");
-});
+/*
 
 
+loadnExecute("https://shakthi.github.io/ThreeJSUniverse/src/sampleMeshModule.js",
+    "defineThreeUniverse", function (contruct) {
+        //debugger;
+        //document.currentScript;
+        scene.add(contruct(THREE));
+    });
 
-
+*/
 
 
