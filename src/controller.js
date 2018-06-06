ac85c905c5b7abe2aba3ef7e7a87853351066d00
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-06-07 00:47:13 +0530

    Modularised controller code

---
+++ src/controller.js
@@ -0,0 +1,139 @@
+import * as THREE from 'three';
+const PointerLockControls = require('three-pointerlock');
+
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var canJump = false;
+
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+var direction = new THREE.Vector3();
+
+
+var controls;
+export function initController(camera) {
+
+    controls = new PointerLockControls(camera);
+
+    var onKeyDown = function (event) {
+
+        switch (event.keyCode) {
+
+            case 38: // up
+            case 87: // w
+                moveForward = true;
+                break;
+
+            case 37: // left
+            case 65: // a
+                moveLeft = true; break;
+
+            case 40: // down
+            case 83: // s
+                moveBackward = true;
+                break;
+
+            case 39: // right
+            case 68: // d
+                moveRight = true;
+                break;
+
+            case 32: // space
+                if (canJump === true) velocity.y += 350;
+                canJump = false;
+                break;
+
+        }
+
+    };
+
+    var onKeyUp = function (event) {
+
+        switch (event.keyCode) {
+
+            case 38: // up
+            case 87: // w
+                moveForward = false;
+                break;
+
+            case 37: // left
+            case 65: // a
+                moveLeft = false;
+                break;
+
+            case 40: // down
+            case 83: // s
+                moveBackward = false;
+                break;
+
+            case 39: // right
+            case 68: // d
+                moveRight = false;
+                break;
+
+        }
+
+    };
+
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
+
+    return controls;
+
+}
+
+let lastPostion;
+
+export function updateController(onObject) {
+
+
+    var time = performance.now();
+    var delta = (time - prevTime) / 1000;
+
+    velocity.x -= velocity.x * 10.0 * delta;
+    velocity.z -= velocity.z * 10.0 * delta;
+
+    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+    direction.z = Number(moveForward) - Number(moveBackward);
+    direction.x = Number(moveLeft) - Number(moveRight);
+    direction.normalize(); // this ensures consistent movements in all directions
+
+    if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+    if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+
+    if (onObject === true) {
+
+        velocity.y = Math.max(0, velocity.y);
+        canJump = true;
+
+    }
+
+    controls.getObject().translateX(velocity.x * delta);
+    controls.getObject().translateY(velocity.y * delta);
+    controls.getObject().translateZ(velocity.z * delta);
+
+    if (controls.getObject().position.y < 10) {
+
+        velocity.y = 0;
+        controls.getObject().position.y = 10;
+
+        canJump = true;
+
+    }
+
+    prevTime = time;
+    let isUpdate=false;
+    if(lastPostion){
+        if (lastPostion.distanceTo(controls.getObject().position)>0.01) {
+            isUpdate = true;
+        }
+    }
+
+
+    lastPostion = controls.getObject().position.clone();
+
+    return isUpdate;
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
-const PointerLockControls = require('three-pointerlock');
 import * as THREE from 'three';
 import { datGUI } from './dat';
 import { initVisibilityDesider } from './visibiltyDesider';
-import {loadUniverseAt } from './objectManager'
+import { loadUniverseAt } from './objectManager'
+import { initController, updateController } from './controller'
 
 
 var camera, scene, renderer, controls;
@@ -79,17 +79,7 @@ if (havePointerLock) {
 
 var controlsEnabled = false;
 
-var moveForward = false;
-var moveBackward = false;
-var moveLeft = false;
-var moveRight = false;
-var canJump = false;
 
-var prevTime = performance.now();
-var velocity = new THREE.Vector3();
-var direction = new THREE.Vector3();
-var vertex = new THREE.Vector3();
-var color = new THREE.Color();
 
 init();
 animate();
@@ -119,73 +109,10 @@ function init() {
     var light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
     light.position.set(0.5, 1, 0.75);
     scene.add(light);
-
-    controls = new PointerLockControls(camera);
+    controls = initController(camera) 
     scene.add(controls.getObject());
-
-    var onKeyDown = function (event) {
-
-        switch (event.keyCode) {
-
-            case 38: // up
-            case 87: // w
-                moveForward = true;
-                break;
-
-            case 37: // left
-            case 65: // a
-                moveLeft = true; break;
-
-            case 40: // down
-            case 83: // s
-                moveBackward = true;
-                break;
-
-            case 39: // right
-            case 68: // d
-                moveRight = true;
-                break;
-
-            case 32: // space
-                if (canJump === true) velocity.y += 350;
-                canJump = false;
-                break;
-
-        }
-
-    };
-
-    var onKeyUp = function (event) {
-
-        switch (event.keyCode) {
-
-            case 38: // up
-            case 87: // w
-                moveForward = false;
-                break;
-
-            case 37: // left
-            case 65: // a
-                moveLeft = false;
-                break;
-
-            case 40: // down
-            case 83: // s
-                moveBackward = false;
-                break;
-
-            case 39: // right
-            case 68: // d
-                moveRight = false;
-                break;
-
-        }
-
-    };
-
-    document.addEventListener('keydown', onKeyDown, false);
-    document.addEventListener('keyup', onKeyUp, false);
-
+    
+  
     raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, - 1, 0), 0, 10);
 
     
@@ -226,42 +153,7 @@ function animate() {
 
         var onObject = intersections.length > 0;
 
-        var time = performance.now();
-        var delta = (time - prevTime) / 1000;
-
-        velocity.x -= velocity.x * 10.0 * delta;
-        velocity.z -= velocity.z * 10.0 * delta;
-
-        velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-
-        direction.z = Number(moveForward) - Number(moveBackward);
-        direction.x = Number(moveLeft) - Number(moveRight);
-        direction.normalize(); // this ensures consistent movements in all directions
-
-        if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
-        if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
-
-        if (onObject === true) {
-
-            velocity.y = Math.max(0, velocity.y);
-            canJump = true;
-
-        }
-
-        controls.getObject().translateX(velocity.x * delta);
-        controls.getObject().translateY(velocity.y * delta);
-        controls.getObject().translateZ(velocity.z * delta);
-
-        if (controls.getObject().position.y < 10) {
-
-            velocity.y = 0;
-            controls.getObject().position.y = 10;
-
-            canJump = true;
-
-        }
-
-        prevTime = time;
+        updateController(onObject);
 
     }
 
