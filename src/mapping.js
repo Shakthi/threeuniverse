6c79fb16dfee746503611ec56a802231f8a082ed
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-06-12 09:24:09 +0530

    Changed relative path of parts

--- src/mapping.js
+++ src/mapping.js
@@ -16,31 +16,31 @@ let maping = [
         position: { x: 0, z: 0 },
         radius: 10000,
         description: "Grass ground with single texture",
-        url: "../../src/universe_parts/grassGround.js",
+        url: "src/universe_parts/grassGround.js",
         credits: "https://threejs.org/examples/#webgl_animation_cloth"
     },
 
     {
         position: { x: 0, z: 0 },
         radius: 10000,
-        url: "../../src/universe_parts/lonetree.js",
+        url: "src/universe_parts/lonetree.js",
     },
     {
         position: { x: 100, z: 100 },
         radius: 10000,
-        url: "../../src/universe_parts/forest.js",
+        url: "src/universe_parts/forest.js",
     },
     {
         position: { x: 0, z: 0 },
         radius: 10000,
-        url: "../../src/universe_parts/directionalLight.js",
+        url: "src/universe_parts/directionalLight.js",
         //credits: "https://threejs.org/examples/#webgl_animation_cloth"
     },    
     {
         position: { x: 0, z: -2700,y:-100 },
         radius: 1000,
         description: "Futeristic ground",
-        url: "../../src/universe_parts/futureGround.js",
+        url: "src/universe_parts/futureGround.js",
     }
 
 
