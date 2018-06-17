bbe5038af416881d59b489d9f8fad64aa00383c9
ashwithags <ashwithags@gmail.com>
2018-06-17 17:41:56 +0530

    Update simpleCube.js
    Added cube part to universe

--- src/universe_parts/simpleCube.js
+++ src/universe_parts/simpleCube.js
@@ -1,4 +1,16 @@
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+
+defineThreeUniverse(function (THREE,options) {
+
+    return new Promise(function (resolve, reject) {
+
+
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+        var cube = new THREE.Mesh( geometry, material );
+
+            resolve(cube);
+
+       
+    });
+});
+
