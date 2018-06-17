4c3906e17f068897352b67c8551551a50245a854
ashwithags <ashwithags@gmail.com>
2018-06-17 17:57:24 +0530

    Scaled cube

--- src/universe_parts/simpleCube.js
+++ src/universe_parts/simpleCube.js
@@ -4,7 +4,7 @@ defineThreeUniverse(function (THREE,options) {
     return new Promise(function (resolve, reject) {
 
 
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var geometry = new THREE.BoxGeometry( 100, 100, 100 );
         var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
         var cube = new THREE.Mesh( geometry, material );
 
