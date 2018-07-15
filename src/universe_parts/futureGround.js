8bc48417fc1054fdbcc242e9c99f50c43a2dd20a
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-07-15 23:46:34 +0530

    Some beautification to future ground

--- src/universe_parts/futureGround.js
+++ src/universe_parts/futureGround.js
@@ -4,7 +4,7 @@ defineThreeUniverse(function (THREE) {
 
     var vertex = new THREE.Vector3();
     var color = new THREE.Color();
-    var floorGeometry = new THREE.PlaneBufferGeometry(2000, 2000, 100, 100);
+    var floorGeometry = new THREE.PlaneBufferGeometry(2000, 2000, 20, 20);
     floorGeometry.rotateX(- Math.PI / 2);
 
     // vertex displacement
@@ -15,28 +15,29 @@ defineThreeUniverse(function (THREE) {
         vertex.fromBufferAttribute(position, i);
 
         vertex.x += Math.random() * 20 - 10;
-        vertex.y += Math.random() * 2;
+        vertex.y += Math.random() * 10;
         vertex.z += Math.random() * 20 - 10;
 
         position.setXYZ(i, vertex.x, vertex.y, vertex.z);
 
     }
 
-    floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+   // floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
 
     let count = floorGeometry.attributes.position.count;
     var colors = [];
 
     for (var i = 0; i < count; i++) {
 
-        color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+        color.setHSL(Math.random() * 0.5 + 0.5, 0.75, Math.random() * 0.1 + 0.75);
         colors.push(color.r, color.g, color.b);
 
     }
 
+    
     floorGeometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
 
-    var floorMaterial = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors });
+    var floorMaterial = new THREE.MeshStandardMaterial({ metalness:0.5,flatShading:true,vertexColors: THREE.VertexColors,shininess:20 });
 
     var floor = new THREE.Mesh(floorGeometry, floorMaterial);
     return floor;
