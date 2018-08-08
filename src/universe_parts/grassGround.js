e866ef6658a60b3f3a292688a1884483dc053ffd
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-08-08 23:55:17 +0530

    Displacement map for nice ground

--- src/universe_parts/grassGround.js
+++ src/universe_parts/grassGround.js
@@ -3,25 +3,60 @@ defineThreeUniverse(function (THREE,options) {
 
     return new Promise(function (resolve, reject) {
 
-        var loader = new THREE.TextureLoader();
-        var mesh = null;
+        var groundTexture = new THREE.TextureLoader().load(options.baseUrl+'resource/grasslight-big.jpg');
+        debugger;
+        var texture = new THREE.TextureLoader().load( 'resource/texture/sECkYCE.png' );
+				var geometry = new THREE.PlaneBufferGeometry( 20000, 20000, 200,200 );
+				var material = new THREE.MeshPhongMaterial({
+          displacementMap: texture,
+          displacementScale:400,
+          displacementBias:-35,
+          aoMap:texture,
+          side: THREE.DoubleSide,
+          map: groundTexture
+        });
+                 mesh = new THREE.Mesh( geometry, material );
+                mesh.rotation.x = - Math.PI / 2;
 
-        var groundTexture = loader.load(options.baseUrl+'resource/grasslight-big.jpg',
-            () => {resolve(mesh)},
-            null,
-            (error) => {reject(error)}
-        );
-        groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
-        groundTexture.repeat.set(25, 25);
-        groundTexture.anisotropy = 16;
+                 resolve(mesh);
 
-        var groundMaterial = new THREE.MeshLambertMaterial({ map: groundTexture });
+//                 mesh.position.set(0,-100,0);
+                groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+                groundTexture.repeat.set(25, 25);
+                groundTexture.anisotropy = 16;
 
-        var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(20000, 20000), groundMaterial);
-        // mesh.position.y = - 250;
-       // mesh.position.y = - 100;
-        mesh.rotation.x = - Math.PI / 2;
-        mesh.receiveShadow = true;
+
+    //     var loader = new THREE.TextureLoader();
+    //     var mesh = null;
+
+    //     var groundTexture = loader.load(options.baseUrl+'resource/grasslight-big.jpg',
+    //         () => {resolve(mesh)},
+    //         null,
+    //         (error) => {reject(error)}
+    //     );
+
+    //     // var displacementMap  = loader.load(options.baseUrl+'resource/texture/sECkYCE.png',()=>{
+    //     //     debugger;
+    //     // });
+
+    //     var displacementMap  = loader.load('https://threejs.org/examples/models/obj/ninja/displacement.jpg',()=>{
+        
+    //     });
+
+        
+    //     // groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+    //     // groundTexture.repeat.set(25, 25);
+    //     // groundTexture.anisotropy = 16;
+
+    //     var groundMaterial = new THREE.MeshStandardMaterial({ map: groundTexture,
+    //         displacementMap:displacementMap,displacementScale:10,
+    //     	displacementBias:  10,  });
+
+    //     var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(20000, 20000,100,100), groundMaterial);
+    //     // mesh.position.y = - 250;
+    //    // mesh.position.y = - 100;
+    //     mesh.rotation.x = - Math.PI / 2;
+         mesh.receiveShadow = true;
 
     });
 
